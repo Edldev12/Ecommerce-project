7109e1a8fce0ba272792ac3601c12dece424fc81
Edlawit Tsegaye <edlawitdigital3@gmail.com>
2026-09-10 16:10:23 +0300

    structure pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import { Header } from "../component/Header";
+
+function HomePage() {
+  return (
+    <>
+      <title>Home Page</title>
+      <Header />
+      <div className="home-page">
+
+      </div>
+    </>
+  )
+}
+export default HomePage;
