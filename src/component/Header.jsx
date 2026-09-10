0a49339141d1f4d0e681626098925985feb39969
Edlawit Tsegaye <edlawitdigital3@gmail.com>
2026-09-10 16:00:40 +0300

    i do header of the ecommerce project

---
+++ src/component/Header.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { useCart } from "../contaxt/useCart";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const { cartQuantity } = useCart();
+
+  return (
+    <header>
+      <div className="header">
+        <div className="left-section">
+          <Link to="/" className="header-link">
+            <img src="/images/logo-white.png" alt="Logo" />
+          </Link>
+        </div>
+
+        <div className="middle-section">
+          <input
+            type="text"
+            placeholder="Search products..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+
+          <button>
+            <img
+              src="/images/search-icon.png"
+              alt="Search"
+              className="search-icon"
+            />
+          </button>
+        </div>
+
+        <div className="right-section">
+          <Link to="/orders" className="header-link">
+            Orders
+          </Link>
+
+          <Link to="/checkout" className="header-link">
+            <div className="cart-wrapper">
+              <img
+                src="/images/cart-icon.png"
+                alt="Cart"
+                className="cart-icon"
+              />
+
+              <span
+                className={`cart-quantity ${cartQuantity >= 10 ? "quantity-large" : ""
+                  }`}
+              >
+                {cartQuantity}
+              </span>
+
+              <div className="cart-text">Cart</div>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
